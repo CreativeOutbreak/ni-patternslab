6ab22dbc4fc153424341ac985977d91d33a5c735
Peter Stewart <pete.stewart@newint.org>
2014-06-11 14:46:02 +0100

    Cofigured grunt watch to check for pattern and scss changes and to reload server. Renamed stylesheet directory and bootstrap.scss inside it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,48 @@
 module.exports = function(grunt) {
 
-  // Configuration 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    shell: {
-        patternlab: {
-          command: "php core/builder.php -gp"
-      }
-    },
-    compass: {                    // Task
-        dist: {                   // Target
-            options: {            // Target options
-                sassDir: 'sass',
-                cssDir: 'css',
-                environment: 'production'
+    // Configuration 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            patternlab: {
+                command: "php core/builder.php -wr"
+            },
+        },
+        compass: {                    // Task
+            dist: {                   // Target
+                options: {            // Target options
+                    sassDir: 'source/scss',
+                    cssDir: 'public/css',
+                    environment: 'production'
+                }
             }
-        }
-    },
-    watch: {
-      html: {
-        files: ['source/_patterns/**/*.mustache', 'source/**/*.json'],
-        tasks: ['shell:patternlab'],
-        options: {
-          spawn: false
-        }
-      }
-    }
-  });
+        },
+        watch: {
+            html: {
+                files: ['source/_patterns/**/*.mustache', 'source/_patterns/**/*.json', 'source/_data/*.json'],
+                tasks: ['shell:patternlab'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+            },
+            scss: {
+                files: ['source/scss/*.scss'],
+                tasks: ['compass:dist'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+           }
 
-  // Plugins
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  
-  // Tasks
-  grunt.registerTask('default', ['watch', 'shell:patternlab']);
-};
+        }
+    });
+
+    // Plugins
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Tasks
+    grunt.registerTask('default', ['watch', 'compass:dist']);
+    };
